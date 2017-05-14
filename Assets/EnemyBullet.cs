ac023f70167320f964e40c69e3e93012c232e3b0
DESKTOP-CSBCMKL\Erin <nern.strudel@gmail.com>
2017-05-14 19:33:29 -0400

    AHHHHHH

---
+++ Assets/EnemyBullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class EnemyBullet : MonoBehaviour {
+
+    public float bulletForce = 200.0f;
+
+    void Start()
+    { 
+
+
+
+    }
+}
