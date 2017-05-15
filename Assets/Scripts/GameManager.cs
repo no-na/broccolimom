1498e1f23e2650fbbbabd6781bbb9b9f0b456fac
Jharv94 <johnharvey94@hotmail.com>
2017-05-14 22:58:17 -0400

    Add files via upload
    FIXED*

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public ShieldControl2 shield;
+    public GameObject shieldObj;
+    public SpriteRenderer shieldRen;
+
+
+
+    void Start()
+    {
+        shield = GameObject.Find("Circle").GetComponent<ShieldControl2>();
+        shieldRen = GameObject.Find("Circle").GetComponent<SpriteRenderer>();
+        Debug.Log(shield);
+        shieldObj = transform.gameObject;
+    }
+
+    public void ShieldHealth (Collider2D target)
+    {
+        
+        Debug.Log(target.gameObject.tag);
+        if (shield.health >= 1 && target.gameObject.tag == "Monster")
+        {
+            shield.health -= 1;
+            Debug.Log(shield.health);
+        }
+        else if (shield.health == 0)
+        {
+            Debug.Log("Zero");
+
+            StartCoroutine(Calling());
+            Debug.Log("Yo");
+            shield.health = 3;
+        }
+    }
+    
+    IEnumerator Calling()
+    {
+        Debug.Log("Coroutine");
+        Invoke("ActivateShield", 2);
+        shieldObj.SetActive(false);
+        shieldRen.enabled = false;
+        Debug.Log("Shield Inactive");
+        yield return 0;
+    }
+
+    void ActivateShield()
+    {
+        
+        shieldObj.SetActive(true);
+        shieldRen.enabled = true;
+        Debug.Log("Shield Active");
+    }
+}
---
+++ Assets/Scripts/ShieldControl2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class ShieldControl2 : MonoBehaviour {
+
+    public GameObject character; // Player is the GameeObject it follows
+    public GameObject shield; // Player is the GameeObject it follows
+    public float accel; //accel changes the speeed at which it rotates
+    public int health = 3;
+
+    void Start()
+    {
+        //shield = GameObject.Find("Circle");
+    }
+
+    void Update()
+    {
+        lall(character);
+        SheildMove();
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        Debug.Log(target.gameObject.tag);
+        GameManager g = GameObject.Find("GameManage").GetComponent<GameManager>();
+        g.ShieldHealth(target);
+    }
+
+
+    void lall(GameObject newParent)
+    {
+        //Same as above, except this makes the player keep its local orientation rather than its global orientation.
+        shield.transform.parent = newParent.transform;
+    }
+
+    void SheildMove()
+    {    
+        
+        transform.localPosition = new Vector3(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), accel * Time.deltaTime); 
+        // this transforms around the object thats moving, based upon your joysticks position, should rotate accordingly
+
+    }
+
+}
+
