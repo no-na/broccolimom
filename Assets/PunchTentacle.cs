8d24c0781131d48932eb5d0bae79b5d8ec9af9d1
Latitude3450\Erin <nern.strudel@gmail.com>
2017-05-29 04:01:42 -0400

    some boss mechanics!

--- Assets/PunchTentacle.cs
+++ Assets/PunchTentacle.cs
@@ -6,17 +6,96 @@ public class PunchTentacle : MonoBehaviour {
 
     private bool isActive = false;
 
+    private Rigidbody2D rb2d;
+
+    //gameobjects for the two characters go here
+    public GameObject target1;
+    public GameObject target2;
+
+    public float speed;
+
+    public float punchInterval;
+    private float punchTimer;
+
+    public float punchLength;
+
+
     // Use this for initialization
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+        punchTimer = punchInterval;
+    }
+
+    void pullBack()
+    {
+        //Vector2 targetDest = new Vector2(5f, transform.position.y);
+        //transform.position = Vector2.MoveTowards(transform.position, targetDest, speed * Time.deltaTime);
+        rb2d.velocity = Vector2.right * speed;
+        Debug.Log("Pulling back");
+    }
+
+    void Punch()
+    {
+        //position the shark
+        //randomly pick one of the two targets
+        GameObject playerTarget = null;
+
+        while (playerTarget == null)
+        {
+
+            int ran = Random.Range(1, 2);
+            if (ran == 1)
+            {
+                playerTarget = target1;
+            }
+            else
+            {
+                playerTarget = target2;
+            }
+
+
+        }
+
+        //get in position
+        transform.position = new Vector3(transform.position.x, playerTarget.transform.position.y, transform.position.z);
+
+        
+
+
+        //zoom.
+        rb2d.velocity = Vector2.left * speed;
+
+        //wait 1.5 seconds
+
+        StartCoroutine(PauseTentacle(punchLength));
+
+
+       // rb2d.velocity = Vector2.zero;
+        
 
     }
+
+    IEnumerator PauseTentacle(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+ 
+        pullBack();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (isActive)
         {
+            punchTimer -= Time.deltaTime;
+
+            if (punchTimer <= 0)
+            {
+                punchTimer = punchInterval;
+                Punch();
+            }
         }
 
     }
--- Assets/SquidHead.cs
+++ Assets/SquidHead.cs
@@ -6,10 +6,21 @@ public class SquidHead : MonoBehaviour {
 
     private bool isActive = false;
 
+    public GameObject target1;
+    public GameObject target2;
+
+    public float spitInterval;
+    private float spitTimer;
+
+    private GameObject spitPoint;
+
+    public GameObject inkPrefab;
+
     // Use this for initialization
     void Start()
     {
-
+        spitPoint = GameObject.Find("SpitPoint");
+        spitTimer = spitInterval;
     }
 
     // Update is called once per frame
@@ -17,9 +28,41 @@ public class SquidHead : MonoBehaviour {
     {
         if (isActive)
         {
+
+
+
+            Vector2 aimPoint = (target1.transform.position + target2.transform.position) / 2;
+
+            //rotate towards the aim point
+            transform.up = aimPoint - (Vector2)transform.position;
+
+
+            spitTimer -= Time.deltaTime;
+            if (spitTimer <= 0)
+            {
+                spitTimer = spitInterval;
+                Spit();
+            }
         }
 
     }
+
+   
+    void Spit()
+    {
+
+        //aim the mouth
+
+
+
+
+        //create the inkbullet
+        GameObject eb = Instantiate(inkPrefab, spitPoint.transform.position, new Quaternion());
+
+        //addforce
+        Rigidbody2D ebrb = eb.GetComponent<Rigidbody2D>(); 
+    }
+
     public void Go()
     {
         isActive = true;
